3145f7fba54aa2be1c882fa718e7114768499a2c
Roma Sava <krampi@Romas-MacBook-Pro.local>
2024-08-11 12:01:13 +0300

    feat: dashboard cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,40 +1,11 @@
-'use client';
-
-import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
-import { Button } from '@/components/ui/button';
+import { DataGrid } from '@/components/data-grid';
 
 const DashboardPage = () => {
-  const { onOpen } = useNewAccount();
-  const { data: accounts, isLoading } = useGetAccounts();
-
-  if (isLoading) {
-    return (
-      <div>
-        Loading...
-      </div>
-    );
-  }
-
   return (
-    <>
-      <Button onClick={() => onOpen()}>
-        Add an account
-      </Button>
-
-      <div>
-        {
-          accounts?.map(account => {
-            return (
-              <div key={account.id}>
-                {account.name}
-              </div>
-            );
-          })
-        }
-      </div>
-    </>
-  );
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid/>
+    </div>
+  )
 };
 
 export default DashboardPage;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { twMerge } from 'tailwind-merge';
 import { type ClassValue, clsx } from 'clsx';
-import { eachDayOfInterval, isSameDay } from 'date-fns';
+import { eachDayOfInterval, format, isSameDay, subDays } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -20,6 +20,18 @@ export function formatCurrency(value: number): string {
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format(value);
+}
+
+export function formatPercentage(value: number, options: { addPrefix?: boolean } = { addPrefix: false }): string {
+  const result = new Intl.NumberFormat('en-US', {
+    style: 'percent',
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+
+  return result;
 }
 
 export function calculatePercentageChange(current: number, previous: number): number {
@@ -51,4 +63,24 @@ export function fillMissingDays(activeDays: Array<{ date: Date; income: number; 
         expenses: 0,
       };
   });
+}
+
+type Period = {
+  to: string | Date | undefined;
+  from: string | Date | undefined;
+}
+
+export function formatDateRange(period?: Period) {
+  const defaultTo = new Date();
+  const defaultFrom = subDays(defaultTo, 30);
+
+  if (!period?.from) {
+    return `${format(defaultFrom, 'LLL dd')} - ${format(defaultTo, 'LLL dd, y')}`;
+  }
+
+  if (period?.to) {
+    return `${format(period.from, 'LLL dd')} - ${format(period.to, 'LLL dd, y')}`;
+  }
+
+  return format(period.from, 'LLL dd, y');
 }
